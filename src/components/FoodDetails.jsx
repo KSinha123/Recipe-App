a900ee4ccb53bc5052e45b07b557f8bbca14bcee
kasturisinha123 <kasturisinha123@gmail.com>
2025-10-30 17:14:32 +0530

    Initial commit

---
+++ src/components/FoodDetails.jsx
@@ -0,0 +1,58 @@
+import { useEffect, useState } from "react";
+import styles from "./fooddetails.module.css";
+import ItemList from "./ItemList";
+export default function FoodDetails({ foodId }) {
+  const [food, setFood] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+  const URL = `https://api.spoonacular.com/recipes/${foodId}/information`;
+  const API_KEY = import.meta.env.VITE_API_KEY;
+  useEffect(() => {
+    async function fetchFood() {
+      const res = await fetch(`${URL}?apiKey=${API_KEY}`);
+      const data = await res.json();
+      console.log(data);
+      setFood(data);
+      setIsLoading(false);
+    }
+    fetchFood();
+  }, [foodId]);
+  return (
+    <div className={styles.recipeCard}>
+      <div>
+        <h1 className={styles.recipeName}>{food.title}</h1>
+        <img className={styles.recipeImage} src={food.image} alt="" />
+        <div className={styles.recipeDetails}>
+          <span>
+            <strong>⏱️{food.readyInMinutes} Minutes</strong>
+          </span>
+          <span>
+            <strong>👨‍👩‍👧‍👦 Serves {food.servings} </strong>
+          </span>
+          <span>
+            <strong>
+              {food.vegetarian ? "🥕Vegetarian" : "🍗Non-Vegetarian"}
+            </strong>
+          </span>
+          <span>{food.vegan ? "Vegan" : ""}</span>
+        </div>
+        <div>
+          💲<span>{food.pricePerServing / 100} Per Serving</span>
+        </div>
+      </div>
+      <h2>Ingredients</h2>
+      <ItemList food={food} isLoading={isLoading} />
+      <h2>Instructions</h2>
+      <div className={styles.recipeInstructions}>
+        <ol>
+          {isLoading ? (
+            <p>Loading....</p>
+          ) : (
+            food.analyzedInstructions[0].steps.map((step) => (
+              <li>{step.step}</li>
+            ))
+          )}
+        </ol>
+      </div>
+    </div>
+  );
+}
